20eab615f4592fffd76cf2349ced3f66eedad925
dj0mlinho <mladenmilinkovic88@gmail.com>
2019-08-07 19:09:41 +0200

    www

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const storage = multer.diskStorage({
     if (isValid) {
       error = null;
     }
-    cb(error, './images/');
+    cb(error, 'data/api/img');
   },
   filename: function(req, file, cb) {
     const name = file.originalname
